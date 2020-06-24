b0a2cb0c56b4f824904e1fcf67add4512eb34654
hanumalasettyrakeshbabu <rakeshbabu393@gmail.com>
2020-06-24 14:59:43 +0530

    Updates

---
+++ precache-manifest.f325f123ca5fead701133098d699d600.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19ff7e0bfe919beb50ef304823d8e4f",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/index.html"
+  },
+  {
+    "revision": "1e5047242efdc7ff5b5e",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4893232c724e058fb15",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/css/main.00f701f0.chunk.css"
+  },
+  {
+    "revision": "1e5047242efdc7ff5b5e",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/js/2.b6720961.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/js/2.b6720961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4893232c724e058fb15",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/js/main.b2723e9b.chunk.js"
+  },
+  {
+    "revision": "b6d7fbe51f481762f433",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/js/runtime-main.151dd39b.js"
+  },
+  {
+    "revision": "96513d2f85fd9584b840f26fcf2c84b0",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/media/Screenshot (53).96513d2f.png"
+  },
+  {
+    "revision": "b8a094a64fc2ebdd53a256ac0b8596c9",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/media/Screenshot (54).b8a094a6.png"
+  },
+  {
+    "revision": "9e79665f839cda04b04831a2f6a6bca0",
+    "url": "/hanumalasettyrakeshbabu/hanumalasettyrakeshbabu.github.io/static/media/Screenshot (55).9e79665f.png"
+  }
+]);
